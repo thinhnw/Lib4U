15da1e034ce8c0a05431a2aeece00f619e3dbeca
GiaThinh1711 <thinhdau1711@gmail.com>
2021-10-25 09:26:20 +0700

    gg

--- Lib4U/App_Start/BundleConfig.cs
+++ Lib4U/App_Start/BundleConfig.cs
@@ -31,6 +31,36 @@ namespace Lib4U
 
             bundles.Add(new ScriptBundle("~/adminlte/js").Include(
              "~/adminlte/js/adminlte.min.js"));
+
+            // add user css
+            bundles.Add(new StyleBundle("~/User/css").Include(
+                "~/Content/User/css/font-awesome.min.css",
+                "~/Content/User/css/jquery.accordion.css",
+                "~/Content/User/css/mmenu.css",
+                "~/Content/User/css/mmenu.positioning.css",
+                "~/Content/User/css/responsivetable.css",
+                "~/Content/User/css/style.css"
+                ));
+            bundles.Add(new ScriptBundle("~/User/js").Include(
+                "~/Content/User/js/accordion.min.js",
+                "~/Content/User/js/bootstrap.min.js",
+                "~/Content/User/js/bxslider.min.js",
+                "~/Content/User/js/carousel.swipe.min.js",
+                "~/Content/User/js/facts.counter.min.js",
+                "~/Content/User/js/google.map.js",
+                "~/Content/User/js/harvey.min.js",
+                "~/Content/User/js/jquery-1.12.4.min.js",
+                "~/Content/User/js/jquery-ui.min.js",
+                "~/Content/User/js/jquery.easing.1.3.js",
+                "~/Content/User/js/main.js",
+                "~/Content/User/js/masonry.min.js",
+                "~/Content/User/js/mixitup.min.js",
+                "~/Content/User/js/mmenu.min.js",
+                "~/Content/User/js/owl.carousel.min.js",
+                "~/Content/User/js/responsive.table.min.js",
+                "~/Content/User/js/responsive.tabs.min.js",
+                "~/Content/User/js/waypoints.min.js"
+                ));
         }
     }
 }
---
+++ Lib4U/Controllers/User/UserHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lib4U.Controllers.User
+{
+    public class UserHomeController : Controller
+    {
+        // GET: Home
+        public ActionResult UserHome()
+        {
+            return View();
+        }
+    }
+}
