3d911cd9255166fe3d05a941005727eb6d81f861
thinhnw <thinhnv0412@gmail.com>
2021-10-26 22:35:02 +0700

    Add Name to Users

--- Lib4U/Models/IdentityModels.cs
+++ Lib4U/Models/IdentityModels.cs
@@ -10,6 +10,8 @@ namespace Lib4U.Models
     public class ApplicationUser : IdentityUser
     {
         public string Address { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -42,5 +44,7 @@ namespace Lib4U.Models
         public System.Data.Entity.DbSet<Lib4U.Models.BookAuthor> BookAuthors { get; set; }
 
         public System.Data.Entity.DbSet<Lib4U.Models.BookGenre> BookGenres { get; set; }
+
+        
     }
 }
